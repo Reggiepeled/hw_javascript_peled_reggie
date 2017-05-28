9a84a001e1ad42fc4e36e292d185b31e07557f66
reggie peled <revital.peled@gmail.com>
2017-05-28 10:45:42 -0400

    add the 'inception' function to main.js

--- js/main.js
+++ js/main.js
@@ -48,3 +48,28 @@ var presidents = [currentPresident,
     fourtyFirstPresident];
 
 console.log(presidents[2].firstName + ' ' + presidents[2].lastName);
+
+function howdy() {
+    console.log('Reggie Peled says hello!');
+}
+howdy();
+
+var numba = function (string) {
+    if (string.length < 7) {
+        console.log('What a short little word!');
+    } else if (string.length > 7) {
+        console.log('I’m sorry, but that’s too many to count.');
+    } else {
+        console.log('What a perfect choice!');
+    }
+};
+
+numba('7');
+numba('seventy');
+numba('university');
+
+function inception(display, favMovie) {
+    display(favMovie);
+}
+
+inception(console.log, 'Maleficent is a fantastic movie.');
